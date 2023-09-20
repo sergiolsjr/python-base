48df1b991b3e5aabc6b44e714aeabe69adf4a62b
Sérgio Jr <sergiolsjr@gmail.com>
2023-09-19 21:38:13 -0300

    add reservas.py

--- reservas.py
+++ reservas.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3
 
-# hotel = [{"codigo": "1", "nome": "Master", "preco": 500},
-#          {"codigo": "2", "nome": "Familia", "preco": 200},
-#          {"codigo": "3", "nome": "Quarto Single", "preco": 100},
-#          {"codigo": "4", "nome": "Simples", "preco": 50}]
-
 print("QUARTOS DISPONÍVEIS")
 print("#"*30)
 print("N -", "   Nome  -", "  Preço  ")
 print("")
 
-for quarto in open("quartos.txt", "r"):
-    hotel = [{"codigo": (quarto.split(",")[0])},
-             {"nome": (quarto.split(",")[1])}, 
-             {"preco": (quarto.split(",")[2])}]
-    print(hotel[0]["codigo"], "-", hotel[1]["nome"], "-", hotel[2]["preco"])
+quartos = {}
+for line in open("quartos.txt", "r"):
+    codigo, nome, preco = line.strip().split(",")
+    quartos[int(codigo)] = {"nome": nome,
+                       "preco": float(preco)}
+print(quartos)
+    
+   
     
     
-    
-
-
-# print("N Quarto - Nome - Preço")
-# for quarto in hotel:
-
-
-
-
-
-
-
-
-
     
 
 
