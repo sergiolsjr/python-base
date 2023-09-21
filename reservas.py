882997bbe80e8463398252ad58b2a6ad013bdd40
Sérgio Jr <sergiolsjr@gmail.com>
2023-09-20 21:30:10 -0300

    Update reservas.py

--- reservas.py
+++ reservas.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 
-print("QUARTOS DISPONÍVEIS")
-print("#"*30)
-print("N -", "   Nome  -", "  Preço  ")
-print("")
-
 quartos = {}
 for line in open("quartos.txt", "r"):
     codigo, nome, preco = line.strip().split(",")
     quartos[int(codigo)] = {"nome": nome,
-                       "preco": float(preco)}
-print(quartos)
+                       "preco": float(preco),
+                       "disponivel": True
+    }
+    print(quartos.items(codigo["disponivel"]))
+    # print(f"{codigo} - {nome} - R$ {preco} - {disponivel}")
     
-   
-    
-    
-    
+print("#"*35)
+nome_cliente = input("Informe seu nome: ")
+num_quarto = int(input("Informe o número do quarto: "))
+qtd_dias = input("Informe a quantidade de dias: ")
 
+for codigo, dados in quartos.items():
+    if num_quarto == codigo:
+        preco_total = dados["preco"] * int(qtd_dias)
+        print("#"*35)
+        print(f"{nome_cliente} - {num_quarto} - R$ {preco_total:.2f}") 
+        with open("reservas.txt", "a") as arquivo:
+            arquivo.write("{},{},{}\n".format(nome_cliente, str(num_quarto), str(preco_total)))
+            #arquivo.writelines(",".join([nome_cliente, str(num_quarto), str(preco_total)]))
+            
+            
+    
+        
 
-#nome_cliente = input("Informe seu nome: ")
     
+    # preco_total = dados["preco"] * int(qtd_dias)
     
-# num_quarto = input("Informe o número do quarto: ")
-# qtd_dias = input("Informe a quantidade de dias: ")
+    # print(f"{nome_cliente} - {num_quarto} - R$ {preco_total}")    
 
 
     
